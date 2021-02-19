c572ba0f9c1323533355d3cc157f5d0641b58323
DocQuantum <shchurgood@gmail.com>
2021-02-19 17:56:13 -0600

    Added ability to specify the up direction

--- Assets/Scripts/FakeFluids.cs
+++ Assets/Scripts/FakeFluids.cs
@@ -21,10 +21,11 @@ public class FakeFluids : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        Debug.Log(_direction);
         d_point = GameObject.CreatePrimitive(PrimitiveType.Cube);
         d_point.transform.localScale = new Vector3(0.05f, 0.05f, 0.05f);
         d_point.transform.parent = transform;
-        d_point.transform.localPosition = Vector3.zero + transform.up;
+        d_point.transform.localPosition = Vector3.zero + GetUpVector(_direction)*0.5f;
         d_mat = d_point.GetComponent<MeshRenderer>().material;
         d_mat.color = Color.red;
     }
@@ -41,16 +42,26 @@ public class FakeFluids : MonoBehaviour
             d_mat.color = Color.green;
     }
 
+    private Vector3 GetUpVector(Direction direction)
+    {
+        Vector3 val = Vector3.zero;
+        if (direction == Direction.red)
+            val = transform.right;
+        else if (direction == Direction.blue)
+            val = transform.forward;
+        else if (direction == Direction.green)
+            val = transform.up;
+        return val;
+    }
     private float GetUpValue(Direction direction)
     {
         float val = 0f;
         if (direction == Direction.red)
-            val = transform.right.x;
+            val = transform.right.y;
         else if (direction == Direction.blue)
-            val = transform.forward.z;
+            val = transform.forward.y;
         else if (direction == Direction.green)
             val = transform.up.y;
-
         return val;
     }
 }
