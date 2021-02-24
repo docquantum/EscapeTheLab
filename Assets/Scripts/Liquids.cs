d8cdd9931ec4f46aaac82a2bbf1ee82da0e10400
Erik Skoog <46847696+euskoog@users.noreply.github.com>
2021-02-24 16:42:45 -0600

    added some UI nonsense

---
+++ Assets/Scripts/Liquids.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Liquids : MonoBehaviour
+{
+
+    /*public GameObject liquid;
+    public GameObject computerScreen;
+    public GameObject notifier;
+
+    Dictionary<string, string> liquids =
+    new Dictionary<string, string>();
+
+    // Update is called once per frame
+    void Update()
+    {
+        // if collision with liquid and another liquid within dict liquids:
+        Liquid collidingLiquid = null; // get from collision
+        onCollision(liquid, collidingLiquid);
+    }
+
+    private onCollision(liquid1, liquid2)
+    {
+        *//*switch {
+            case 1:
+                do something;
+        }*//*
+    }*/
+}
