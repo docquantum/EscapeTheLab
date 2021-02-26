f48735132355017df3339bb8817fa03942929a5b
DocQuantum <shchurgood@gmail.com>
2021-02-26 12:05:26 -0600

    Improved liquid class to allow serialization

--- Assets/Scripts/Liquid.cs
+++ Assets/Scripts/Liquid.cs
@@ -2,11 +2,30 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Liquid
+/// <summary>
+/// Liquid class type which should be added and set to a container which should have a liquid.
+/// </summary>
+public class Liquid : MonoBehaviour
 {
-    public string Name { get; set; }
-    public Color Color { get; set; }
-    public string Description { get; set; }
+    [SerializeField] private string _name = "default";
+    [SerializeField] private Color _color = Color.white;
+    [SerializeField] [TextArea] private string _description = "default description";
+
+    public string Name
+    {
+        get { return _name; }
+        set { _name = value; }
+    }
+    public Color Color
+    { 
+        get { return _color; } 
+        set { _color = value; } 
+    }
+    public string Description 
+    { 
+        get { return _description; } 
+        set { _description = value; } 
+    }
 
     Liquid(string name, Color color, string descrition)
     {
